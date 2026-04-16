48ad02d8427cc105b501b930cb2c93892e71e641
Jan Burkhardt <burkhardt.jan.august@gmail.com>
2026-04-16 11:55:42 +0200

    Unit-Tests hinzugefügt

---
+++ README.md
@@ -0,0 +1,2 @@
+# Best Buy 2
+The second part of a Shopping Demo Project for MSIT Masterschool
---
+++ test_product.py
@@ -0,0 +1,43 @@
+import pytest
+import products
+
+
+def test_create_product():
+    test_product = products.Product("Test 1", price=250, quantity=500)
+    # assert test_product.name == "Test 1"
+    # assert test_product.price == 250
+    # assert test_product.quantity == 500
+    assert hasattr(test_product, "name")
+    assert getattr(test_product, "name") == "Test 1"
+    assert hasattr(test_product, "price")
+    assert getattr(test_product, "price") == 250
+    assert hasattr(test_product, "quantity")
+    assert getattr(test_product, "quantity") == 500
+
+
+def test_create_product_empty_name():
+    with pytest.raises(Exception) as e:
+        test_product = products.Product("", price=250, quantity=500)
+
+
+def test_create_product_negative_price():
+    with pytest.raises(ValueError) as e:
+        test_product = products.Product("Test", price=-250, quantity=500)
+
+
+def test_product_becomes_inactive():
+    test_product = products.Product("Test", price=250, quantity=500)
+    test_product.buy(500)
+    assert test_product.is_active() == False
+
+
+def test_buy_modifies_quantity():
+    test_product = products.Product("Test", price=250, quantity=500)
+    test_product.buy(300)
+    assert test_product.get_quantity() == 200
+
+
+def test_buy_too_many_quantity():
+    with pytest.raises(Exception) as e:
+        test_product = products.Product("Test", price=250, quantity=500)
+        test_product.buy(600)
